0c44aae71707ea154b860ca22ef814f2384d2332
hunterbowcutt <jhunterb789@gmail.com>
2019-03-28 11:02:28 -0600

    inital commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Photos from './photos';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <Photos/>
+      </div>
+    );
+  }
+}
